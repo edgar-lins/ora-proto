88bfcfa58f4ff6949fb546558d5ee1a28a8c0f3f
Edgar Lins <linss.edgar@gmail.com>
2025-10-22 10:57:31 -0300

    chore: initial project setup

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('ORA API running 🚀');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
