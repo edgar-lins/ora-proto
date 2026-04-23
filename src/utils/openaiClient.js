506c2619b58ae819895cef28fa53dc9b0fae1f81
Edgar Lins <linss.edgar@gmail.com>
2026-04-23 18:07:07 -0300

    fix: dotenv.config() no openaiClient antes de instanciar OpenAI

--- src/utils/openaiClient.js
+++ src/utils/openaiClient.js
@@ -1,4 +1,6 @@
 import OpenAI, { toFile } from "openai";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 export { toFile };
